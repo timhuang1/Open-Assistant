b1bb532282724190bd4c20f7fc32e6d477f6f034
timhuang1 <timhuangxt@gmail.com>
2023-06-07 16:51:57 +0800

    2023年 6月 7日 星期三 16时51分57秒 CST

--- model/model_training/custom_datasets/qa_datasets.py
+++ model/model_training/custom_datasets/qa_datasets.py
@@ -495,8 +495,9 @@ class LocalDialogue(Dataset):
         self.mode = mode
         self.name = dataset_name
         assert (input_file_path := kwargs.get("input_file_path")) is not None, "Loading LocalDialogue ds requires passing input_file_path"
-        dataset = load_dataset(data_dir, data_files=input_file_path, cache_dir=cache_dir)
+        # dataset = load_dataset(data_dir, data_files=input_file_path, cache_dir=cache_dir)
         # self.pairs = self.process_dialog_samples(dataset["train"])
+        dataset = [json.load(ln) for ln in open(os.path.join(data_dir, input_file_path))]
         self.pairs = list()
         for data in dataset:
             if (qa := Vicuna.process_vicuna_conversations(data, input_max_length=input_max_length)) is not None:
